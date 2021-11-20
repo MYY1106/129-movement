94f90413c861670276f8764f1fb8aaf37ba321c2
MYY1106 <361598804@qq.com>
2021-11-21 01:14:28 +0800

    chore: test

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,15 @@ const History = lazy(() => import('./views/History'))
 const End = lazy(() => import('./views/End'))
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Home}></Route>
-				<Route exact path="/history" component={History}></Route>
-				<Route exact path="/end" component={End}></Route>
-			</Switch>
-		</Router>
-	)
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/history" component={History}></Route>
+                <Route exact path="/end" component={End}></Route>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App
