29b764099d384cbabad5b547b4aa3ea28b801c69
MYY1106 <361598804@qq.com>
2021-11-21 00:28:27 +0800

    chore: test

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-    return (
-        <div>
-            <Link to="/history">
-                历史回顾
-            </Link>
-        </div>
-    )
+	return (
+		<div>
+			<Link to="/history">历史回顾</Link>
+		</div>
+	)
 }
