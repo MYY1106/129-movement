0db1802efb2fec8d56effa09cf8eff801d8ad8ff
Markovchaloven <953531767@qq.com>
2021-11-20 21:40:41 +0800

    chore: initial commit

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+declare module "*.module.less" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.less";
+declare module "*.png";
+declare module "*.gif";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.css";
+declare module "*.js";
+declare module "*.mp3";
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Home() {
+    return (
+        <div>
+            <Link to="/history">
+                历史回顾
+            </Link>
+        </div>
+    )
+}
