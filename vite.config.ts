e816bd8a0b9ad39d33ab81909f079ca54954dc49
MYY1106 <361598804@qq.com>
2021-11-22 00:08:42 +0800

    chore: init commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-    build:{
-        outDir:'build'
-    }
+    plugins: [react()],
+    build: {
+        outDir: 'build',
+    },
 })
