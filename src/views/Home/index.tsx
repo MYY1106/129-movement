9a12e8d252c2e6362bf61ba32d2614f9fd5961d8
MYY1106 <361598804@qq.com>
2021-11-28 23:37:24 +0800

    feat: 完成首页

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
+import styles from '../../assets/style/home.module.less'
 
 export default function Home() {
-	return (
-		<div>
-			<Link to="/history">历史回顾</Link>
-		</div>
-	)
+    return (
+        <div className={styles['home-container']}>
+            <div className={styles['home-button']}></div>
+            {/* <Link to="/history">历史回顾</Link> */}
+            <div className={styles['home-testbox']}></div>
+        </div>
+    )
 }
