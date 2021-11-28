801c472335150b9094e0c848b487060151fa2c05
MYY1106 <361598804@qq.com>
2021-11-28 22:15:27 +0800

    chore: 配置postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssPxToViewport from 'postcss-px-to-viewport'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
         outDir: 'build',
+    },
+    css: {
+        postcss: {
+            plugins: [postcssPxToViewport, autoprefixer],
+        },
     },
 })
