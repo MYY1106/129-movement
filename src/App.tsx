03647238f3fbddd17d9d3f9486f0968af0186ff3
MYY1106 <361598804@qq.com>
2021-11-30 20:33:06 +0800

    feat: 增加loading路由

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,14 @@ import History from './views/History'
 
 import End from './views/End'
 
+import Loading from './views/Loading'
+
 function App() {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Home}></Route>
+                <Route exact path="/loading" component={Loading}></Route>
                 <Route exact path="/history" component={History}></Route>
                 <Route exact path="/end" component={End}></Route>
             </Switch>
