fc4a0874784ee7df0b11bc80482b8f14417ebe60
MYY1106 <361598804@qq.com>
2021-11-30 20:34:26 +0800

    feat: 大致完成数字滚动效果组件

---
+++ src/components/ScrollNumber/index.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import styles from '../../assets/style/scrollNumber.module.less'
+
+const ScrollNumber: React.FC<ScrollNumberProps> = props => {
+    let ListNode: HTMLUListElement | null
+    const style: Style = {
+        '--oldi': props.oldNum,
+        '--i': props.num,
+        '--color': 'skyblue',
+        '--width': '30px',
+        '--height': '25px',
+        '--duration': props.speed + 's',
+    }
+    useEffect(() => {
+        const addClassTimer = setTimeout(() => {
+            ListNode?.classList.add(styles['number-animation'])
+        }, 2000)
+
+        const removeClassTimer = setTimeout(() => {
+            ListNode?.classList.remove(styles['number-animation'])
+            ListNode?.classList.add(styles['number-no-animation'])
+        }, 2000 + props.lastTime * 1000)
+
+        return () => {
+            clearTimeout(addClassTimer)
+            clearTimeout(removeClassTimer)
+        }
+    }, [])
+    return (
+        <div>
+            <ul
+                className={styles['scroll-number-list']}
+                style={style}
+                ref={currentNode => (ListNode = currentNode)}
+            >
+                <li>0</li>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>0</li>
+            </ul>
+        </div>
+    )
+}
+export default ScrollNumber
