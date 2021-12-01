0851a3e14132076b88a1a807083d040e629e509a
Markovchaloven <2053492@brunel.ac.uk>
2021-12-01 19:21:33 +0800

    chore: add new configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssPxToViewport from 'postcss-px-to-viewport'
 import autoprefixer from 'autoprefixer'
+import path from 'path'
 
 const loderPostcssPxToViewport = postcssPxToViewport({
     viewportWidth: 375,
@@ -11,6 +12,13 @@ const loderPostcssPxToViewport = postcssPxToViewport({
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: "assets", replacement: path.resolve(__dirname, 'src/assets')
+            }
+        ]
+    },
     build: {
         outDir: 'build',
     },
@@ -19,4 +27,7 @@ export default defineConfig({
             plugins: [loderPostcssPxToViewport, autoprefixer],
         },
     },
+    server: {
+        host: '0.0.0.0'
+    }
 })
