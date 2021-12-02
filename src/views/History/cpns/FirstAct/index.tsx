452fa9ff81ca32041a3c3817d4a5f309d9108531
MYY1106 <361598804@qq.com>
2021-12-02 19:17:17 +0800

    feat: 增加第一幕

---
+++ src/views/History/cpns/FirstAct/index.tsx
@@ -0,0 +1,6 @@
+import Map from './pages/Map'
+
+const FirstAct: React.FC = () => {
+    return <Map />
+}
+export default FirstAct
---
+++ src/views/History/cpns/FirstAct/pages/Map.tsx
@@ -0,0 +1,11 @@
+import TypedLines from '../../../../../components/TypedLines'
+
+const FirstAct: React.FC = () => {
+    return (
+        <div>
+            <TypedLines lines="日本的魔爪伸向华北地图，于1935" />
+            <TypedLines lines="年发动华北事变" lastTimeTypedWords={17} />
+        </div>
+    )
+}
+export default FirstAct
