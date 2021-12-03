bbc8111a44047132f5f1f99d2a2a5fd80712fa87
MYY1106 <361598804@qq.com>
2021-12-04 00:45:45 +0800

    feat: 第一幕的contract组件大致完成

--- src/views/History/cpns/FirstAct/pages/Contract.tsx
+++ src/views/History/cpns/FirstAct/pages/Contract.tsx
@@ -1,13 +1,13 @@
 import styles from '../../../../../assets/style/FirstAct/contract.module.less'
 import 'animate.css'
+
 const Contract: React.FC = () => {
     return (
         <div className={styles['contract-container']}>
-            <div className={styles['sentences-box']}>
-                {`国民党政府签下“何梅协定”和
-                  “秦土协定”把包括平津在内的
-                  河北、察哈尔两省的大部分主权
-                  奉送给日本。`}
+            <div
+                className={`${styles['sentences-box']} animate__animated animate__fadeIn`}
+            >
+                {`国民党政府签下“何梅协定”和\n“秦土协定”把包括平津在内的\n河北、察哈尔两省的大部分主权\n奉送给日本。`}
             </div>
             <div className={styles['contract']}>
                 <div
