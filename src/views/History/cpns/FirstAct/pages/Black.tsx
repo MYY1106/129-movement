af0fe7b1f200a1de7c42641b3074d7626cb3a40c
MYY1106 <361598804@qq.com>
2021-12-03 22:23:39 +0800

    feat: 大致完成第一幕的black组件

--- src/views/History/cpns/FirstAct/pages/Black.tsx
+++ src/views/History/cpns/FirstAct/pages/Black.tsx
@@ -1,4 +1,17 @@
+import styles from '../../../../../assets/style/FirstAct/Black.module.less'
+import '../../../../../assets/style/font.less'
+
 const FirstAct: React.FC = () => {
-    return <div>hello</div>
+    return (
+        <div className={styles['background']}>
+            <div className={styles['sentences-box']}>
+                {`“九一八”事变之后，日本帝国
+                主义加紧侵略中国。他们在东北
+                地区推行殖民地化统治的同时， 
+                把侵略魔爪一步步伸向华北，民
+                族危机日益严重。`}
+            </div>
+        </div>
+    )
 }
 export default FirstAct
