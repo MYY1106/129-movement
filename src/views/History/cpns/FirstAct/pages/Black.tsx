7b08852d1a537966d1042ccca345827e2a43d4c1
MYY1106 <361598804@qq.com>
2021-12-04 00:45:19 +0800

    feat: 第一幕的black组件大致完成

--- src/views/History/cpns/FirstAct/pages/Black.tsx
+++ src/views/History/cpns/FirstAct/pages/Black.tsx
@@ -1,10 +1,16 @@
-import styles from '../../../../../assets/style/FirstAct/Black.module.less'
+import { useEffect } from 'react'
+import { transition } from '../../../utils'
+import styles from '../../../../../assets/style/FirstAct/black.module.less'
 import '../../../../../assets/style/font.less'
+import 'animate.css'
 
 const FirstAct: React.FC = () => {
+    let divNode: HTMLDivElement | null
     return (
         <div className={styles['background']}>
-            <div className={styles['sentences-box']}>
+            <div
+                className={`${styles['sentences-box']} animate__animated animate__fadeIn`}
+            >
                 {`“九一八”事变之后，日本帝国
                 主义加紧侵略中国。他们在东北
                 地区推行殖民地化统治的同时， 
