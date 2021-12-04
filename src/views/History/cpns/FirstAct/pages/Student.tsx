1dfbacf795859d28d6099193d0398e0f3758a1e4
MYY1106 <361598804@qq.com>
2021-12-05 01:09:41 +0800

    feat: 完成第一幕的student组件

--- src/views/History/cpns/FirstAct/pages/Student.tsx
+++ src/views/History/cpns/FirstAct/pages/Student.tsx
@@ -1,10 +1,44 @@
+import { useEffect } from 'react'
 import styles from '../../../../../assets/style/FirstAct/student.module.less'
 import 'animate.css'
 
-const Student: React.FC = () => {
+const Student: React.FC<PageProps> = ({ changeAct }) => {
+    let divNode: HTMLDivElement | null,
+        handsNode: HTMLDivElement | null,
+        angerNode: HTMLDivElement | null
+    useEffect(() => {
+        new Promise(resolve => {
+            setTimeout(() => {
+                handsNode?.classList.add(styles['show-hands'])
+                resolve('show hands success!!')
+            }, 500)
+        })
+            .then(() => {
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        angerNode?.classList.add(styles['show-anger'])
+                        resolve('show anger success!!')
+                    }, 500)
+                })
+            })
+            .then(() => {
+                setTimeout(() => {
+                    divNode?.addEventListener('click', () => {
+                        ;(changeAct as Function)(2000)
+                    })
+                }, 1000)
+            })
+    })
+
     return (
-        <div className={styles['student-container']}>
-            <div className={styles['hands']}>
+        <div
+            className={`${styles['student-container']} animate__animated animate__fadeIn`}
+            ref={currentNode => (divNode = currentNode)}
+        >
+            <div
+                className={styles['hands']}
+                ref={currentNode => (handsNode = currentNode)}
+            >
                 <div className={styles['sentences-box']}>
                     {`国民党政府坚持不抵抗政策，在
                   民族危亡时刻，北平爱国学生感
@@ -12,6 +46,10 @@ const Student: React.FC = () => {
                   平静的书桌了！”`}
                 </div>
             </div>
+            <div
+                className={styles['anger']}
+                ref={currentNode => (angerNode = currentNode)}
+            ></div>
         </div>
     )
 }
