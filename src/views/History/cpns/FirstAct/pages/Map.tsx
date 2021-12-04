3c784318e66e15cc9a0fd0bef171f2a57194d18e
MYY1106 <361598804@qq.com>
2021-12-05 01:08:43 +0800

    feat: 完成第一幕的map组件

--- src/views/History/cpns/FirstAct/pages/Map.tsx
+++ src/views/History/cpns/FirstAct/pages/Map.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'
 import TypedLines from '../../../../../components/TypedLines'
 import styles from '../../../../../assets/style/FirstAct/map.module.less'
 import 'animate.css'
 
-const Map: React.FC = () => {
+const Map: React.FC<PageProps> = ({ changeActive }) => {
+    let divNode: HTMLDivElement | null
+    useEffect(() => {
+        setTimeout(() => {
+            divNode!.addEventListener('click', () => {
+                changeActive('isContractActive', true)
+                changeActive('isMapActive', false)
+            })
+        }, 4000)
+    }, [])
     return (
-        <div className={styles['map-container']}>
+        <div
+            className={`${styles['map-container']} animate__animated animate__fadeIn'
+            `}
+            ref={currentNode => (divNode = currentNode)}
+        >
             <div className={styles['sentences-box']}>
                 <TypedLines lines="日本的魔爪伸向华北地图，于1935" />
                 <TypedLines lines="年发动华北事变。" lastTimeTypedWords={17} />
