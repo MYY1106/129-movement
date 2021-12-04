88b3fa4402c856f686c65f4f5273fd1780a6da18
MYY1106 <361598804@qq.com>
2021-12-05 01:16:22 +0800

    feat: 完成第一幕的index组件

--- src/views/History/cpns/FirstAct/index.tsx
+++ src/views/History/cpns/FirstAct/index.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from 'react'
 import Map from './pages/Map'
 import Black from './pages/Black'
+import Contract from './pages/Contract'
+import Student from './pages/Student'
 
-const FirstAct: React.FC = () => {
-    return <Black />
+const FirstAct: React.FC<FirstActProps> = ({ nextAct }) => {
+    let timer: NodeJS.Timeout
+    let [actActiveConfig, setActActiveConfig] = useState({
+        isBlackActive: true,
+        isMapActive: false,
+        isContractActive: false,
+        isStudentActive: false,
+    })
+    const changeNotActive = (pageActive: string, isActive: boolean) => {
+        setActActiveConfig(actActiveConfig => ({
+            ...actActiveConfig,
+            [pageActive]: isActive,
+        }))
+    }
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [])
+
+    return (
+        <div style={{ backgroundColor: 'black', zIndex: '-100' }}>
+            {(() => {
+                if (actActiveConfig.isBlackActive)
+                    return <Black changeActive={changeNotActive} />
+                if (actActiveConfig.isMapActive)
+                    return <Map changeActive={changeNotActive} />
+                if (actActiveConfig.isContractActive)
+                    return <Contract changeActive={changeNotActive} />
+                if (actActiveConfig.isStudentActive)
+                    return (
+                        <Student
+                            changeActive={changeNotActive}
+                            changeAct={nextAct}
+                        />
+                    )
+            })()}
+        </div>
+    )
 }
 export default FirstAct
