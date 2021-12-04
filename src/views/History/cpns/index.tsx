520383a07b35781b3dd35c72d396502f3d05f6f3
MYY1106 <361598804@qq.com>
2021-12-05 01:21:09 +0800

    chore: 整合history组件

--- src/views/History/cpns/index.tsx
+++ src/views/History/cpns/index.tsx
@@ -1,6 +1,7 @@
 import FirstAct from './FirstAct'
-import SecondAct from "./SecondAct"
-import FourthAct from "./FourthAct"
+import SecondAct from './SecondAct'
+import ThirdAct from './ThirdAct'
+import FourthAct from './FourthAct'
+import FifthAct from './FifthAct'
 import SixthAct from './SixthAct'
-
-export { FirstAct, SecondAct, FourthAct, SixthAct }
+export { FirstAct, SecondAct, ThirdAct, FourthAct, FifthAct, SixthAct }
--- src/views/History/index.tsx
+++ src/views/History/index.tsx
@@ -1,55 +1,62 @@
 import { Link } from 'react-router-dom'
 import { useRef, useEffect, useState, FC } from 'react'
-import { transition } from "./utils/index"
-// import { FirstAct, SectionAct, ThridAct, FourthAct, FifthAct, SixthAct } from "./cpns"
-import { SecondAct, FourthAct, SixthAct } from "./cpns"
+import { transition } from './utils/index'
+import {
+    FirstAct,
+    SecondAct,
+    ThirdAct,
+    FourthAct,
+    FifthAct,
+    SixthAct,
+} from './cpns'
 
 enum actions {
-	one, two, three, four, five, six
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
 }
 
-
 const History: FC = () => {
-	const [act, setAct] = useState<actions>(0)
-	const [time, setTime] = useState<number>(2000)
+    const [act, setAct] = useState<actions>(0)
+    const [time, setTime] = useState<number>(2000)
 
-	let loading = true;
+    let loading = true
 
-	const changeAct = (t: number) => {
-		if (!loading) {
-			setTime(t)
-			act >= 5 ? setAct(0) : setAct(act + 1)
-		}
-	}
+    const changeAct = (t: number) => {
+        if (!loading) {
+            setTime(t)
+            act >= 5 ? setAct(0) : setAct(act + 1)
+        }
+    }
 
-	useEffect(() => {
-		setTimeout(() => { loading = false; }, time)
-	})
+    useEffect(() => {
+        setTimeout(() => {
+            loading = false
+        }, time)
+    })
 
-	return (<>
-		{(() => {
-			if (act === 0) {
-				return (
-					<SecondAct nextAct={changeAct} />
-				)
-			} else if (act === 1) {
-				return (
-					<FourthAct nextAct={changeAct} />
-				)
-			} else if (act === 2) {
-				return (
-					<SixthAct />
-				)
-			}
-			// else if (act === 3) {
-			// 	return ()
-			// } else if (act === 4) {
-			// 	return ()
-			// } else if (act === 5) {
-			// 	return ()
-			// }
-		})()}
-	</>)
+    return (
+        <>
+            {(() => {
+                if (act === 0) {
+                    return <FirstAct nextAct={changeAct} />
+                } else if (act === 1) {
+                    return <SecondAct nextAct={changeAct} />
+                } else if (act === 2) {
+                    return <ThirdAct nextAct={changeAct} />
+                } else if (act === 3) {
+                    return <FourthAct nextAct={changeAct} />
+                } else if (act === 4) {
+                    return <FifthAct nextAct={changeAct} />
+                } else if (act === 5) {
+                    return <SixthAct />
+                }
+            })()}
+        </>
+    )
 }
 
 export default History
