cef074bcea4877639f1c0992b84e26b31c0b8046
MYY1106 <361598804@qq.com>
2021-12-07 18:30:56 +0800

    chore: 修改背景音乐

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -1,32 +1,31 @@
-import { ReactElement, useState, useRef, FC, forwardRef } from "react"
+import { ReactElement, useState, useRef, FC, forwardRef } from 'react'
 import style from 'assets/style/music.module.less'
-import bgm from 'assets/music/找回我的精神.mp3'
+import bgm from 'assets/music/bgm.mp3'
 
 interface IMusic {
-    loop: boolean,
+    loop: boolean
 }
 
 const Music: FC<IMusic> = ({ loop = true }): ReactElement => {
     // 控制音乐组件
     let useControl = useRef<HTMLDivElement>(null)
 
-
     // 创建音频，实现循环播放
     let audio = new Audio(bgm)
     audio.loop = loop
 
-    let touchTimes = 0;
-    let isPlay = false;
-    let deg = 0;
+    let touchTimes = 0
+    let isPlay = false
+    let deg = 0
 
     // 用户触碰页面模拟自动播放
     document.addEventListener('touchstart', autoPlayMusic)
     // 自动播放函数
     function autoPlayMusic() {
         audio.play()
-        touchTimes++;
+        touchTimes++
         if (touchTimes == 2) {
-            document.removeEventListener('touchstart', autoPlayMusic);
+            document.removeEventListener('touchstart', autoPlayMusic)
             isPlay = true
             window.requestAnimationFrame(playAnimation)
         }
@@ -34,7 +33,7 @@ const Music: FC<IMusic> = ({ loop = true }): ReactElement => {
 
     // 旋转动画，递归调用
     function playAnimation() {
-        deg = deg + 0.3;
+        deg = deg + 0.3
         useControl.current!.style.transform = `rotate(${deg}deg)`
         if (isPlay) {
             window.requestAnimationFrame(playAnimation)
@@ -45,16 +44,20 @@ const Music: FC<IMusic> = ({ loop = true }): ReactElement => {
     const controlAudio = () => {
         console.log('test')
         if (!isPlay) {
-            audio.play();
+            audio.play()
             window.requestAnimationFrame(playAnimation)
+        } else {
+            audio.pause()
         }
-        else {
-            audio.pause();
-        }
-        isPlay = !isPlay;
+        isPlay = !isPlay
     }
     return (
-        <div className={style.music} ref={useControl} onClick={controlAudio}></div>)
+        <div
+            className={style.music}
+            ref={useControl}
+            onClick={controlAudio}
+        ></div>
+    )
 }
 
-export default Music;
+export default Music
