db07e993c6ef3587b0e7e7978d14857beccf4a9b
hypocritical-7u <hypocritical7u@gmail.com>
2021-12-08 20:12:32 +0800

    chore: change some words

--- src/views/History/cpns/FourthAct/pages/Black.tsx
+++ src/views/History/cpns/FourthAct/pages/Black.tsx
@@ -5,7 +5,7 @@ import styles from "../four.module.less"
 const Black: FC<fnProps> = ({ changeAct }) => {
     return (<div className={styles["four-bg-black"] + " " + styles["four-bg"]} onClick={() => changeAct(4500)}>
         <article className={styles["four-words"] + " animate__animated animate__fadeIn"}>
-            “一二·九”的抗日怒吼，震撼
+            “一·二·九”的抗日怒吼，震撼
             <br />
             了古都北平，全国各阶级人民纷
             <br />
